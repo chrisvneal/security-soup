ba7b97f0cdbf73693831cc0da413940778b76e0f
Chris Neal <chrisvneal@gmail.com>
2022-04-23 02:34:55 -1000

    List all site components on App page as headers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./styles.scss";
 
-export default function App() {
+const App = () => {
   return (
     <div className="App">
       <h1>Security Soup</h1>
-      <h2>Welcome to Security Soup!</h2>
+      <h2>Tasks</h2>
+      <h2>Calendar</h2>
+      <h2>Notes</h2>
+      <h2>Officers</h2>
+      <h2>Site Stats</h2>
+      <h2>Random Topic Generator</h2>
     </div>
   );
-}
+};
+
+export default App;
