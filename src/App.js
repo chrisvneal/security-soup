4b22ab395f04285bc543244b26e0848d557a7156
Chris Neal <chrisvneal@gmail.com>
2022-04-23 02:40:56 -1000

    Add section tag around individual security sections

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import "./styles.scss";
 const App = () => {
   return (
     <div className="App">
-      <h1>Security Soup</h1>
-      <h2>Tasks</h2>
-      <h2>Calendar</h2>
-      <h2>Notes</h2>
-      <h2>Officers</h2>
-      <h2>Site Stats</h2>
-      <h2>Random Topic Generator</h2>
+      <section>
+        <h1>Security Soup</h1>
+      </section>
+      <section>
+        <h2>Tasks</h2>
+        <h2>Events</h2>
+      </section>
+      <section>
+        <h2>Notes</h2>
+      </section>
+      <section>
+        <h2>Officers</h2>
+      </section>
+      <section>
+        <h2>Site Stats</h2>
+      </section>
+      <section>
+        <h2>Random Topic Generator</h2>
+      </section>
     </div>
   );
 };
