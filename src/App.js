04d5b9d15f0a27fb63acc261ac53f4c3e7fc4b7e
Chris Neal <chrisvneal@gmail.com>
2022-04-23 02:46:21 -1000

    Add semantic elements

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import "./styles.scss";
 const App = () => {
   return (
     <div className="App">
-      <section>
+      <header>
         <h1>Security Soup</h1>
-      </section>
-      <section>
-        <h2>Tasks</h2>
-        <h2>Events</h2>
-      </section>
-      <section>
-        <h2>Notes</h2>
-      </section>
-      <section>
-        <h2>Officers</h2>
-      </section>
-      <section>
-        <h2>Site Stats</h2>
-      </section>
-      <section>
-        <h2>Random Topic Generator</h2>
-      </section>
+      </header>
+      <main>
+        <section>
+          <h2>Tasks</h2>
+          <h2>Events</h2>
+        </section>
+        <section>
+          <h2>Notes</h2>
+        </section>
+        <section>
+          <h2>Officers</h2>
+        </section>
+        <section>
+          <h2>Site Stats</h2>
+        </section>
+        <section>
+          <h2>Random Topic Generator</h2>
+        </section>
+      </main>
+      <footer>Footer</footer>
     </div>
   );
 };
