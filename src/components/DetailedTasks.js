8f1e84288747c23d093f927d4adfc1d02bded3fd
Chris Neal <chrisvneal@gmail.com>
2022-04-25 20:15:41 -1000

    Create DetailedTasks components

---
+++ src/components/DetailedTasks.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DetailedTasks = () => {
+  return (
+    <section className="section-border detailed-tasks">
+      <h2>Detailed Tasks</h2>
+    </section>
+  );
+};
+
+export default DetailedTasks;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,16 @@
 import React from "react";
+import DetailedTasks from "./DetailedTasks";
 
 const Tasks = ({ tasks }) => {
   const taskList = tasks.map((task) => <li key={task.id}>{task.title}</li>);
   return (
-    <section className="section-border tasks">
-      <h2>Tasks</h2>
-      <ul className="tasks-list">{taskList}</ul>
-    </section>
+    <>
+      <section className="section-border tasks">
+        <h2>Tasks</h2>
+        <ul className="tasks-list">{taskList}</ul>
+      </section>
+      <DetailedTasks />
+    </>
   );
 };
 
