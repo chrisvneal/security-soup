478a44c19a724d04094003cd91c2fe8e24da8717
Chris Neal <chrisvneal@gmail.com>
2022-04-25 20:09:37 -1000

    Create SiteStatus component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Officers from "./components/Officers";
 import Tasks from "./components/Tasks";
 import SiteHeader from "./components/Site/SiteHeader";
 import SiteFooter from "./components/Site/SiteFooter";
+import SiteStatus from "./components/SiteStatus";
 import { officers, supTasks } from "./data";
 
 import "./styles.scss";
@@ -13,15 +14,13 @@ const App = () => {
     <div className="App">
       <SiteHeader />
       <main>
+        <SiteStatus />
         <Tasks tasks={supTasks} />
 
         <Notes />
 
         <Officers officers={officers} />
 
-        <section className="section-border">
-          <h2>Site Stats</h2>
-        </section>
         <section className="section-border">
           <h2>Random Topic Generator</h2>
           <button>Generate topic</button>
---
+++ src/components/SiteStatus.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SiteStatus = () => {
+  return (
+    <section className=" site-status section-border">
+      <h2>Site Status</h2>
+    </section>
+  );
+};
+
+export default SiteStatus;
