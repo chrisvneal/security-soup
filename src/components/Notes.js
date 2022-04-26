3d94487d1c7f9c0815705ff7a983ab175d785cf4
Chris Neal <chrisvneal@gmail.com>
2022-04-25 23:12:07 -1000

    Style Notes section

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -3,10 +3,10 @@ import RecentNotes from "./RecentNotes";
 
 const Notes = () => {
   return (
-    <section className="section-border notes">
+    <section className="notes">
       <h2>Notes</h2>
       <textarea className="section-border note-space"></textarea>
-      <button>Save note</button>
+      <button className="savenote-btn">Save note</button>
       <RecentNotes />
     </section>
   );
