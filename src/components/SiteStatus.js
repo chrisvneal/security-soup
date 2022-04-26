34453d083723ff5fc5f8b76a9048c14c791035ba
Chris Neal <chrisvneal@gmail.com>
2022-04-25 23:00:24 -1000

    Add line-height to officer and tasks list items

--- src/components/SiteStatus.js
+++ src/components/SiteStatus.js
@@ -7,17 +7,19 @@ const SiteStatus = () => {
       <ul>
         <li className="site">
           <h3 className="site-name">Rancho Relaxo</h3>
-          <span>Hours of Operation: 0600 - 1800</span>
+
           <ul>
+            <li>Hours of Operation: 0600 - 1800</li>
             <li>Supervisor: Armani Vuitton</li>
             <li>20 Officers</li>
             <li>Secured: armed</li>
           </ul>
         </li>
         <li className="site">
-          <h3 className="site-name">Biowaste Unlimited</h3>
-          <span>Hours: 0430 - 1600</span>
+          <h3 className="site-name">Biowaste Management</h3>
+
           <ul>
+            <li>Hours: 0430 - 1600</li>
             <li>Supervisor: Fila Rivera</li>
             <li>10 Officers</li>
             <li>Secured: unarmed</li>
