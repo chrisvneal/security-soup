a0f6c54be22a920ef403260f0f013130dfc80caa
Chris Neal <chrisvneal@gmail.com>
2022-04-25 21:59:54 -1000

    Creat initial styling for random topic.

--- src/components/RandomTopicGenerator.js
+++ src/components/RandomTopicGenerator.js
@@ -4,6 +4,10 @@ const RandomTopicGenerator = () => {
   return (
     <section className="section-border random-topic-generator">
       <h2>Random Topic Generator</h2>
+      <p className="random-topic">
+        Proper wanding of visitors at Echo Post especially if they are a pain in
+        the ass
+      </p>
       <button>Generate topic</button>
     </section>
   );
