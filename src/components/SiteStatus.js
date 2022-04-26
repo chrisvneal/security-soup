90637bd9242302ad83847b2d3f90101930270e0b
Chris Neal <chrisvneal@gmail.com>
2022-04-25 22:16:58 -1000

    Add fake site data

--- src/components/SiteStatus.js
+++ src/components/SiteStatus.js
@@ -4,6 +4,26 @@ const SiteStatus = () => {
   return (
     <section className=" site-status section-border">
       <h2>Site Status</h2>
+      <ul>
+        <li className="site">
+          <h3 classNme="site-name">Rancho Relaxo</h3>
+          <span>Hours of Operation: 0600 - 1800</span>
+          <ul>
+            <li>Supervisor: Armani Vuitton</li>
+            <li>20 Officers</li>
+            <li>Secured: armed</li>
+          </ul>
+        </li>
+        <li className="site">
+          <h3 classNme="site-name">Biowaste Unlimited</h3>
+          <span>Hours: 0430 - 1600</span>
+          <ul>
+            <li>Supervisor: Fila Rivera</li>
+            <li>10 Officers</li>
+            <li>Secured: unarmed</li>
+          </ul>
+        </li>
+      </ul>
     </section>
   );
 };
