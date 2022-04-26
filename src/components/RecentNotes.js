228d41a86ae9ca3cd5da3a55efeedcbc3fef4d75
Chris Neal <chrisvneal@gmail.com>
2022-04-25 21:11:11 -1000

    Create RecentNotes component

---
+++ src/components/RecentNotes.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RecentNotes = () => {
+  return (
+    <section className="section-border recent-notes">
+      <h2>Recent Notes</h2>
+    </section>
+  );
+};
+
+export default RecentNotes;
