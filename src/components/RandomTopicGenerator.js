00e48d9a565c3bed93214d31002770961da3fa8d
Chris Neal <chrisvneal@gmail.com>
2022-04-27 20:42:40 -1000

    Use useState to hold current topic

--- src/components/RandomTopicGenerator.js
+++ src/components/RandomTopicGenerator.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { securityTopics } from "../data";
 
 const RandomTopicGenerator = () => {
+  const [currentTopic, setCurrentTopic] = useState(0);
+
   const generateRandomTopic = () => {
-    let randNum = Math.floor(Math.random() * securityTopics.length);
+    setCurrentTopic(Math.floor(Math.random() * securityTopics.length));
 
-    const randomTopic = securityTopics[randNum];
-
-    document.querySelector(".random-topic").textContent = randomTopic;
+    document.querySelector(".random-topic").textContent =
+      securityTopics[currentTopic];
   };
+
   return (
     <section className="random-topic-generator">
       <h2>Random Topic Generator</h2>
-      <p className="random-topic">{securityTopics[0]}</p>
+      <p className="random-topic">{securityTopics[currentTopic]}</p>
       <button onClick={generateRandomTopic} className="randomtopic-btn">
         Generate topic
       </button>
