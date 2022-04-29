f8084c601bc0f0f0e7b2b74a094913ea3e9737f1
Chris Neal <chrisvneal@gmail.com>
2022-04-29 03:08:22 -1000

    Create form around textarea for Notes

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -2,12 +2,20 @@ import React from "react";
 import RecentNotes from "./RecentNotes";
 
 const Notes = () => {
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
   const recentNotes = ["take out the trash", "wash the clothes"];
   return (
     <section className="notes">
       <h2>Notes</h2>
-      <textarea className="note-space"></textarea>
-      <button className="savenote-btn">Save note</button>
+      <form action="#" onSubmit={onFormSubmit}>
+        <textarea className="note-space"></textarea>
+        <button type="submit" className="savenote-btn">
+          Save note
+        </button>
+      </form>
       <RecentNotes recentNotes={recentNotes} />
     </section>
   );
