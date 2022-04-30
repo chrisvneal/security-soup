5dd938e8160d0baa6b018839a31ffd030c39dc15
Chris Neal <chrisvneal@gmail.com>
2022-04-30 01:20:42 -1000

    Set up time element under header

--- src/components/Site/SiteHeader.js
+++ src/components/Site/SiteHeader.js
@@ -2,14 +2,17 @@ import React from "react";
 
 const SiteHeader = () => {
   return (
-    <header className="site-header">
-      <h1 className="site-title">Security Soup</h1>
-      <img
-        src="https://pngimg.com/uploads/soup/soup_PNG1.png"
-        alt="soup"
-        className="soup"
-      />
-    </header>
+    <>
+      <header className="site-header">
+        <h1 className="site-title">Security Soup</h1>
+        <img
+          src="https://pngimg.com/uploads/soup/soup_PNG1.png"
+          alt="soup"
+          className="soup"
+        />
+      </header>
+      <div className="date-time"></div>
+    </>
   );
 };
 
