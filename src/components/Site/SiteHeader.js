92ecedcc0506aca254c6005fbf0b858ec6b78720
Chris Neal <chrisvneal@gmail.com>
2022-04-29 23:54:38 -1000

    Add image of soup

--- src/components/Site/SiteHeader.js
+++ src/components/Site/SiteHeader.js
@@ -4,6 +4,11 @@ const SiteHeader = () => {
   return (
     <header className="site-header">
       <h1 className="site-title">Security Soup</h1>
+      <img
+        src="https://pngimg.com/uploads/soup/soup_PNG1.png"
+        alt="soup"
+        className="soup"
+      />
     </header>
   );
 };
