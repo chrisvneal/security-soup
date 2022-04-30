02a65715bc89ef806fc3d65b30310d9f71c2614d
Chris Neal <chrisvneal@gmail.com>
2022-04-29 23:49:57 -1000

    Allow topic generator to cycle through topics

--- src/components/SecurityTopicGenerator.js
+++ src/components/SecurityTopicGenerator.js
@@ -2,24 +2,23 @@ import React, { useState } from "react";
 import { securityTopics } from "../data";
 
 const SecurityTopicGenerator = () => {
-  const [currentTopic, setCurrentTopic] = useState(0);
+  const [securityTopic, setSecurityTopic] = useState(0);
   // console.log("current topic: " + currentTopic);
 
   const showNextTopic = () => {
-    if (currentTopic === securityTopics.length - 1) {
-      setCurrentTopic(0);
+    if (securityTopic === securityTopics.length - 1) {
+      setSecurityTopic(0);
     } else {
-      setCurrentTopic((prevState) => prevState + 1);
+      setSecurityTopic((prevState) => prevState + 1);
     }
 
-    document.querySelector(".security-topic").textContent =
-      securityTopics[currentTopic];
+    document.querySelector(".security-topic").textContent = securityTopic;
   };
 
   return (
     <section className="security-topic-generator">
       <h2>Security Topic Generator</h2>
-      <p className="security-topic">{securityTopics[currentTopic]}</p>
+      <p className="security-topic">{securityTopics[securityTopic]}</p>
       <button onClick={showNextTopic} className="securitytopic-btn">
         Generate topic
       </button>
