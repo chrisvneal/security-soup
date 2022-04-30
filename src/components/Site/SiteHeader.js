dd172a594a0347e1c5227078b509dfe1f7930a77
Chris Neal <chrisvneal@gmail.com>
2022-04-30 02:07:51 -1000

    Adjust SiteHeader margin bottom

--- src/components/Site/SiteHeader.js
+++ src/components/Site/SiteHeader.js
@@ -2,17 +2,14 @@ import React from "react";
 
 const SiteHeader = () => {
   return (
-    <>
-      <header className="site-header">
-        <h1 className="site-title">Security Soup</h1>
-        <img
-          src="https://pngimg.com/uploads/soup/soup_PNG1.png"
-          alt="soup"
-          className="soup"
-        />
-      </header>
-      <div className="date-time"></div>
-    </>
+    <header className="site-header">
+      <h1 className="site-title">Security Soup</h1>
+      <img
+        src="https://pngimg.com/uploads/soup/soup_PNG1.png"
+        alt="soup"
+        className="soup"
+      />
+    </header>
   );
 };
 
