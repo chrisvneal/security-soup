6e748b0ee260ab41428ac01ba487e015e1e21262
Chris Neal <chrisvneal@gmail.com>
2022-04-29 23:33:43 -1000

    Add Github profile to footer link

--- src/components/Site/SiteFooter.js
+++ src/components/Site/SiteFooter.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const SiteFooter = () => {
-  return <footer>Chris Neal &copy; 2022</footer>;
+  return (
+    <footer>
+      <a rel="noreferrer" href="https://github.com/chrisvneal" target="_blank">
+        Chris Neal
+      </a>{" "}
+      &copy; 2022
+    </footer>
+  );
 };
 
 export default SiteFooter;
