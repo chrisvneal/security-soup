9467873746df12772be24a5456d9cbf2dc2775ae
Chris Neal <chrisvneal@gmail.com>
2022-04-30 15:47:37 -1000

    Create place in header to display date

--- src/components/Site/SiteHeader.js
+++ src/components/Site/SiteHeader.js
@@ -3,12 +3,15 @@ import React from "react";
 const SiteHeader = () => {
   return (
     <header className="site-header">
-      <h1 className="site-title">Security Soup</h1>
-      <img
-        src="https://pngimg.com/uploads/soup/soup_PNG1.png"
-        alt="soup"
-        className="soup"
-      />
+      <div className="brand-logo">
+        <h1 className="site-title">Security Soup</h1>
+        <img
+          src="https://pngimg.com/uploads/soup/soup_PNG1.png"
+          alt="soup"
+          className="soup"
+        />
+      </div>
+      <div className="header-date">Date</div>
     </header>
   );
 };
