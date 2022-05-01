65ce71b4350415d062b37a5f4dccaa8e788d7b6d
Chris Neal <chrisvneal@gmail.com>
2022-04-30 21:22:12 -1000

    Create OfficerDetails component

---
+++ src/components/OfficerDetails.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OfficerDetails = () => {
+  return (
+    <div className="officer-details">
+      <p>Officer Details</p>
+    </div>
+  );
+};
+
+export default OfficerDetails;
