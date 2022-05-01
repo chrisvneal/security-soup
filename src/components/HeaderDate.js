71cc2aa0a7aee58461e26ab7c1746274bebd7412
Chris Neal <chrisvneal@gmail.com>
2022-04-30 15:59:35 -1000

    Display date

--- src/components/HeaderDate.js
+++ src/components/HeaderDate.js
@@ -1,7 +1,35 @@
 import React from "react";
 
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
 const HeaderDate = () => {
-  return <div className="header-date">Date</div>;
+  const time = new Date();
+
+  const month = time.getMonth();
+  const day = time.getDate();
+  const year = time.getFullYear();
+  //   console.log(months[month]);
+
+  const date = `${day} ${months[month]} ${year}`;
+
+  //   setInterval(() => {
+  //     console.log(time);
+  //   }, 1000);
+
+  return <div className="header-date">{date}</div>;
 };
 
 export default HeaderDate;
