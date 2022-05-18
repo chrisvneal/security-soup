e97f12a355d62bfda9e5ff99d18007bc83cf1053
Chris Neal <chrisvneal@gmail.com>
2022-05-18 00:35:16 -1000

    Move all TaskInput component code to Tasks component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,7 +1,114 @@
-import React from "react";
-import TaskInput from "./TaskInput";
+import React, { useState, useRef } from "react";
+
+const supTasks = [
+  {
+    title: "Tell Jim to sign paperwork",
+    completed: false,
+    id: 1,
+  },
+  {
+    title: "Help Manager with New hire Orientation",
+    completed: false,
+    id: 2,
+  },
+  {
+    title: "Establish Training for next 1st Watch Shift",
+    completed: false,
+    id: 3,
+  },
+  {
+    title: "Load PPE into back / Admin office with Manager",
+    completed: false,
+    id: 4,
+  },
+];
 
 const Tasks = () => {
+  const listUl = useRef();
+  const inputRef = useRef();
+  const taskInput = useRef();
+  const trashIconRef = useRef();
+
+  const [textInput, setTextInput] = useState("");
+  // put an <li></li> around each task string
+  const makeTaskListItem = (text) => {
+    const li = document.createElement("li");
+    li.textContent = text;
+    return li;
+  };
+
+  // clear input field
+  const clearInput = () => {
+    setTextInput("");
+  };
+
+  const [taskItems, setTaskItems] = useState(() => {
+    return supTasks.map((task) => {
+      makeTaskListItem(task.title);
+    });
+  });
+  // const taskInput = document.querySelector(".task-input");
+
+  // insert task list items in array
+  const insertTaskItem = (taskItem) => {
+    // taskItems.push(taskItem);
+
+    setTaskItems(...taskItems, taskItem);
+  };
+
+  // appends each task to Tasks List
+  const placeItems = () => {
+    taskItems.forEach((task) => {
+      listUl.append(task);
+    });
+  };
+
+  const createTask = (task) => {
+    // insert new list itemm into array
+    insertTaskItem(makeTaskListItem(task));
+
+    // append list items to list (show in task list)
+    placeItems();
+
+    // clear the field
+    clearInput();
+  };
+
+  // functions to reset task and empty the list
+  const resetTasks = () => {
+    // taskItems = [];
+    setTaskItems([]);
+  };
+
+  // const emptyList = () => {
+  //   Array.from(document.querySelectorAll(".task-list ul li")).forEach(
+  //     (item) => {
+  //       item.remove();
+  //     }
+  //   );
+  // };
+
+  if (inputRef.current) {
+    // Press 'Enter' on input to enter task
+    inputRef.current.addEventListener("keypress", (e) => {
+      if (e.key === "Enter") {
+        createTask(e.target.value);
+
+        // hide task input
+        taskInput.current.classList.add("hidden");
+      }
+    });
+  } else {
+    console.log("Input element not found!");
+  }
+
+  if (trashIconRef.current) {
+    trashIconRef.current.addEventListener("click", () => {
+      resetTasks();
+      emptyList();
+    });
+  }
+
   // store all tasks in list items
   // let taskItems = [];
 
@@ -18,7 +125,7 @@ const Tasks = () => {
         Tasks: <span className="task-count">5</span>
       </h2>
       <div className="tasks-list">
-        <ul></ul>
+        <ul ref={listUl}></ul>
       </div>
 
       <div className="tasks-header">
@@ -26,7 +133,7 @@ const Tasks = () => {
           onClick={() => {
             toggleTaskInput();
             // focus on input
-            document.querySelector(".task-input input").focus();
+            inputRef.current.focus();
           }}
           className="add-btn">
           <img
@@ -34,12 +141,22 @@ const Tasks = () => {
             alt=""
           />
         </div>
-        <div className="trash-icon">
+        <div ef={trashIconRef} className="trash-icon">
           <img src="./images/delete.png" />
         </div>
       </div>
 
-      <TaskInput />
+      <div ref={taskInput} className="task-input hidden">
+        <input
+          onChange={(e) => {
+            setTextInput(e.target.value);
+          }}
+          ref={inputRef}
+          type="text"
+          placeholder="Enter task ..."
+          value={textInput}
+        />
+      </div>
     </section>
   );
 };
